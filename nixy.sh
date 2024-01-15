908ba1eaec197e26bdb0b06e0a2ddb33b0312a7b
NIXY Project <152050497+nixyproject-cfd@users.noreply.github.com>
2024-01-15 17:11:39 +0700

    NIXY STORE

--- nixy.sh
+++ nixy.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/nixyproject-cfd/vip/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/nixyproject-cfd/siren/main/nixy/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
